be7da7ee458b3071386ce34cc172f7741c5ad89f
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-16 19:38:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Driver {
   constructor(name, startDate) {
     this.name = name;
-    this.startDate = new Date();
+    this.startDate = new Date(startDate);
   }
 
   yearsExperienceFromBeginningOf(year) {
